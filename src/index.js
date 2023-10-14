7612e6c3be6af08b2075294e6058e1c5323c1169
adriannahdbell@gmail.com <120890577+thecodedcosmos@users.noreply.github.com>
2023-10-13 21:08:53 -0700

    Added Javascript for typing in and seeing list.

--- src/index.js
+++ src/index.js
@@ -9,4 +9,14 @@ function addTask(){
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
     }
+    inputBox.value = "";
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+    }
+}, false);
